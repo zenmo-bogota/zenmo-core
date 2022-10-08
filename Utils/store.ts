fc8dd62a279fe8b38b5a4267e097eb517c3c4dfb
Jackson Harris III <apollo@Jacksons-MacBook-Pro.local>
2022-10-08 11:43:58 -0500

    init store

--- Utils/store.ts
+++ Utils/store.ts
@@ -1,25 +1,35 @@
 // store/store.js
 import create from 'zustand';
-import { persist } from 'zustand/middleware'
+import { persist } from 'zustand/middleware';
 // create store
-const useStore = create(persist(set, get) => ({
-  // State Variables
-  wallet: null,
+const useStore = create(
+  persist(
+    (set, get) => ({
+      // State Variables
+      wallet: null,
 
-  //State methods
-  setWallet: (userWallet) => {
-    set({ wallet: userWallet });
-  },
+      //State methods
+      setWallet: (userWallet) => {
+        set({ wallet: userWallet });
+      },
 
-  //examples
+      //examples
 
-  // color: 'white',
+      // color: 'white',
 
-  // changeColor: () =>
-  //   set((state) => ({ color: state.color === 'white' ? '#212529' : 'white' })),
-  // fetch: async (pond) => {
-  //   const response = await fetch(pond)
-  //   set({ fishies: await response.json() })
-  // },
-})));
+      // changeColor: () =>
+      //   set((state) => ({ color: state.color === 'white' ? '#212529' : 'white' })),
+      // fetch: async (pond) => {
+      //   const response = await fetch(pond)
+      //   set({ fishies: await response.json() })
+      // },
+    }),
+    {
+      name: 'zenmo-storage', // unique name
+      // getStorage: () => sessionStorage, // (optional) by default, 'localStorage' is used
+      serialize: (state) => btoa(JSON.stringify(state)),
+      deserialize: (str) => JSON.parse(atob(str)),
+    }
+  )
+);
 export default useStore;
