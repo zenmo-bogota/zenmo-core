53cf73fffc22ff17537fc5759668ef6ad110228b
Jackson Harris III <apollo@Jacksons-MacBook-Pro.local>
2022-10-08 19:51:30 -0500

    remove ipfs issue

--- Utils/ipfs-utils.ts
+++ Utils/ipfs-utils.ts
@@ -1,16 +1,17 @@
 import * as Name from 'w3name';
-import { create } from 'ipfs-core';
+// import { create } from 'ipfs-core';
 
 export const ipfsNode = async () => {
-  try {
-    const node = await create();
-    const nodeId = await node.id();
-    const nodeVersion = await node.version();
-    const nodeIsOnline = node.isOnline();
-    return node;
-  } catch (err) {
-    console.log(err);
-  }
+  return 'work in progress';
+  // try {
+  //   const node = await create();
+  //   const nodeId = await node.id();
+  //   const nodeVersion = await node.version();
+  //   const nodeIsOnline = node.isOnline();
+  //   return node;
+  // } catch (err) {
+  //   console.log(err);
+  // }
 };
 
 const utilsNode = ipfsNode();
